e78f7e15132976dad6a2e8e7a0decaecd642b140
webanalyticsninja <30498530+webanalyticsninja@users.noreply.github.com>
2019-04-24 15:57:47 +0100

    Update 00 Cookie specific path.js

--- 00 Cookie specific path.js
+++ 00 Cookie specific path.js
@@ -2,22 +2,42 @@
 
 //Specific paths that need to be treated with this special case where utag.data is not set
 
+/*
 shopdisney.co.uk/offers
 shopdisney.de/zauberhafte-angebote
 shopdisney.fr/offres
 shopdisney.es/ofertas
 shopdisney.it/offerte
-
+*/
 
 var pathName = window.location.pathname; //set the pathname of url
 function setPfmCookie(){
 var cookievalue = sitedl.listing.productFindingLogic; //set the value of the product_finding_method to "cookievalue"
-document.cookie="pfm="+cookievalue+";path="+pathName+"";
+document.cookie = "pfm="+cookievalue+";path="+pathName+"";
 }
 
 if ((pathName.indexOf("/offers") > -1) || (pathName.indexOf("/zauberhafte-angebote") > -1) || (pathName.indexOf("/offres") > -1) || (pathName.indexOf("/ofertas") > -1) || (pathName.indexOf("/offerte") > -1)) {
-
+  if (document.cookie.indexOf("pfm") > -1) {
+    b.product_finding_method = getCookie("pfm")
+  } else {
+    setPfmCookie() 
+  }
 
 }
 
-document.cookie
+//function that read the cookie, if it exists. 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+} 
